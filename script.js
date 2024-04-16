45163ddb72ce80d7e6e12d68cb791e68fdb96869
Meralis <lucy.klim.30@gmail.com>
2024-04-16 09:39:47 +0300

    feat: add email and phone fields and regex validation

--- script.js
+++ script.js
@@ -21,26 +21,18 @@ const regexes = {
 buyButton.addEventListener('click', () => form.classList.add('visible'));
 form.addEventListener('input', handleChange);
 
-function buildWarningSpan(className, content) {
+function buildAddInsertWarning(warningMsg, input) {
     const span = document.createElement('span');
-    span.textContent = content;
-    span.classList.add(className);
-    return span;
+    span.textContent = warningMsg.message;
+    span.classList.add(warningMsg.className);
+    input.parentElement.insertBefore(span, input);
 }
 
 function validateInput(input) {
     const regex = regexes[input.name] || null;
     let isInputValid = regex ? regex.test(input.value) : true;
-    let incorrectFieldWarningSpan = input.parentElement.querySelector(incorrectWarningMsg.selector);
     if (!isInputValid) {
-        if (!incorrectFieldWarningSpan) {
-            incorrectFieldWarningSpan = buildWarningSpan(incorrectWarningMsg.className, incorrectWarningMsg.message);
-            input.parentElement.insertBefore(incorrectFieldWarningSpan, input);
-        }
-    } else {
-        if (incorrectFieldWarningSpan) {
-            incorrectFieldWarningSpan.remove();
-        }
+        buildAddInsertWarning(incorrectWarningMsg, input);
     }
     return isInputValid;
 }
@@ -48,41 +40,25 @@ function validateInput(input) {
 function handleChange(e) {
     if (e.target.tagName === 'INPUT') {
         const input = e.target;
-        let emptyFieldWarningSpan = input.parentElement.querySelector(emptyWarningMsg.selector);
-        let incorrectFieldWarningSpan = input.parentElement.querySelector(incorrectWarningMsg.selector);
-        if (!input.value && !emptyFieldWarningSpan) {
-            if (incorrectFieldWarningSpan) {
-                incorrectFieldWarningSpan.remove();
-            }
-            emptyFieldWarningSpan = buildWarningSpan(emptyWarningMsg.className, emptyWarningMsg.message);
-            input.parentElement.insertBefore(emptyFieldWarningSpan, input);
+        input.parentElement.querySelectorAll(`${emptyWarningMsg.selector}, ${incorrectWarningMsg.selector}`).forEach(warning => warning.remove());
+        if (!input.value) {
+            buildAddInsertWarning(emptyWarningMsg, input);
         }
         if (input.value) {
-            if (emptyFieldWarningSpan) {
-                emptyFieldWarningSpan.remove();
-            }
             validateInput(input);
         }
     }
 }
 
 function validateForm() {
-    const emptyWarnings = document.querySelectorAll(emptyWarningMsg.selector);
-    emptyWarnings.forEach(warning => warning.remove());
+    document.querySelectorAll(`${emptyWarningMsg.selector}, ${incorrectWarningMsg.selector}`).forEach(warning => warning.remove());
     let isFormValid = true;
     inputs.forEach(input => {
-        let emptyFieldWarningSpan = input.parentElement.querySelector(emptyWarningMsg.selector);
-        if (emptyFieldWarningSpan) {
-            emptyFieldWarningSpan.remove();
-        }
         if (!input.value) {
-            emptyFieldWarningSpan = buildWarningSpan(emptyWarningMsg.className, emptyWarningMsg.message);
-            input.parentElement.insertBefore(emptyFieldWarningSpan, input);
+            buildAddInsertWarning(emptyWarningMsg, input);
             isFormValid = false;
         } else {
-            if (!validateInput(input)) {
-                isFormValid = false;
-            }
+            isFormValid = validateInput(input) && isFormValid;
         }
     });
     const incorrectWarnings = document.querySelectorAll(incorrectWarningMsg.selector);
